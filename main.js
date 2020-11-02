c2629b3954b66dbe892a4071a59c980b4ace6438
elyesskharroubi <medelyesskharroubi@outlook.com>
2020-11-02 16:21:35 +0100

    second commit

--- main.js
+++ main.js
@@ -35,46 +35,79 @@ const redHeart = (e) => e.classList.toggle("red-heart");
 const btns = document.getElementsByClassName("buyBtn");
 Array.from(btns).map((e) => {
   e.addEventListener("click", () => {
-    var container = document.querySelector(".box");
+    let unitP = e.nextElementSibling.innerHTML;
+    totalPrice.innerHTML =
+      parseInt(totalPrice.innerHTML) + parseInt(unitP.replace(" DT", ""));
+    // var container = document.querySelector(".box");
 
+    var price = e.nextElementSibling.innerHTML;
     var name =
       e.parentElement.previousElementSibling.previousElementSibling
         .previousElementSibling.innerHTML;
-    var price = e.nextElementSibling.innerHTML;
-    var cartRow = document.createElement("ul");
-    cartRow.classList.add("cartRow");
-    var cartName = document.createElement("li");
-    cartName.classList.add("cartItemName");
-    cartName.innerHTML = name;
-    var cartPrice = document.createElement("li");
-    cartPrice.classList.add("cartItemPrice");
-    cartPrice.innerHTML = price;
-    var cartQuantity = document.createElement("li");
-    cartQuantity.classList.add("cartItemQuantity");
-    var quantityInput = document.createElement("input");
-    quantityInput.setAttribute("type", "number");
-    quantityInput.setAttribute("id", "quantityInput");
-    quantityInput.setAttribute("value", "1");
-    var deleteItem = document.createElement("li");
-    var delBtn = document.createElement("button");
-    delBtn.classList.add("removeBtn", "btn", "btn-danger");
-    delBtn.innerHTML = "X";
-    cartRow.append(cartName);
-    cartRow.append(cartPrice);
-    cartQuantity.append(quantityInput);
-    cartRow.append(cartQuantity);
-    deleteItem.append(delBtn);
-    cartRow.append(deleteItem);
-    container.insertBefore(cartRow, container.children[3]);
+    let structur = `<ul class="cartRow"><li class="cartItemName">${name}</li><li class="cartItemPrice">${price}</li><li class="unit">${price}</li><li class="cartItemQuantity"><input type="number" class="quantityInput" value="1"></li><li><button class="removeBtn btn btn-danger">X</button></li></ul>`;
+
+    document.querySelector(".list-items").innerHTML =
+      document.querySelector(".list-items").innerHTML + structur;
+    // var cartRow = document.createElement("ul");
+    // cartRow.classList.add("cartRow");
+    // var cartName = document.createElement("li");
+    // cartName.classList.add("cartItemName");
+    // cartName.innerHTML = name;
+    // var cartPrice = document.createElement("li");
+    // cartPrice.classList.add("cartItemPrice");
+    // cartPrice.innerHTML = price;
+    // var cartQuantity = document.createElement("li");
+    // cartQuantity.classList.add("cartItemQuantity");
+    // var quantityInput = document.createElement("input");
+    // quantityInput.setAttribute("type", "number");
+    // quantityInput.setAttribute("id", "quantityInput");
+    // quantityInput.setAttribute("value", "1");
+    // var deleteItem = document.createElement("li");
+    // var delBtn = document.createElement("button");
+    // delBtn.classList.add("removeBtn", "btn", "btn-danger");
+    // delBtn.innerHTML = "X";
+    // cartRow.append(cartName);
+    // cartRow.append(cartPrice);
+    // cartQuantity.append(quantityInput);
+    // cartRow.append(cartQuantity);
+    // deleteItem.append(delBtn);
+    // cartRow.append(deleteItem);
+    // container.insertBefore(cartRow, container.children[3]);
 
     // REMOVING ITEMS FROM CART //
+
+    let qtyInputs = document.querySelectorAll(".quantityInput");
+    Array.from(qtyInputs).map((el) => {
+      el.addEventListener("change", (e) => {
+        let qty = e.target.value;
+        let unit =
+          el.parentElement.previousElementSibling.previousElementSibling
+            .innerHTML;
+        totalPrice.innerHTML =
+          parseInt(totalPrice.innerHTML) + parseInt(unit.replace(" DT", ""));
+        if (qty <= 0) {
+          e.target.value = 1;
+        } else {
+          el.parentElement.previousElementSibling.innerHTML =
+            parseInt(unit.replace(" DT", "")) * parseInt(qty) + " DT";
+          // totalPrice.innerHTML =
+          //   parseInt(totalPrice.innerHTML) + parseInt(unit.replace(" DT", ""));
+        }
+      });
+    });
 
     let delItem = document.querySelectorAll(".removeBtn");
     Array.from(delItem).map((el) =>
       el.addEventListener("click", function () {
         el.parentElement.parentElement.remove();
         totalPrice.innerHTML =
-          parseInt(totalPrice.innerHTML) - parseInt(price.replace(" DT", ""));
+          parseInt(totalPrice.innerHTML) -
+          parseInt(
+            el.parentElement.previousElementSibling.previousElementSibling.innerHTML.replace(
+              " DT",
+              ""
+            )
+          );
       })
     );
 
@@ -85,7 +118,5 @@ Array.from(btns).map((e) => {
     //   () => (price = parseInt(price.replace(" DT", "")) * parsInt(quantity))
     // );
     // TOTAL PRICE //
-    totalPrice.innerHTML =
-      parseInt(totalPrice.innerHTML) + parseInt(price.replace(" DT", ""));
   });
 });
